011b02ce2ba60f4f2d5ecd8d41f2451668ae9a23
Oliver Thordeman <o.thordeman@live.se>
2021-08-26 10:01:54 +0100

    initial commit

---
+++ src/Editor.js
@@ -0,0 +1,42 @@
+import React, {Component, useState} from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+
+function Editor() {
+
+	const [datas, setDatas] = useState([])
+
+    function getDataToConsoleLog() {
+        console.log(datas)
+    }
+
+
+    return (
+    
+        <div className="App">
+        <div> <button onClick = {getDataToConsoleLog}>Spara</button></div>
+        <h2>Editor komponent: CKEditor</h2>
+        <CKEditor
+            editor={ ClassicEditor }
+            data="<p>Skriv något!</p>"
+            
+            onReady={ editor => {
+                // You can store the "editor" and use when it is needed.
+                console.log( 'Editor is ready to use!', editor );
+            } }
+            onChange={ ( event, editor ) => {
+                /* const data = editor.getData(); */
+                setDatas(editor.getData())
+            } }
+            onBlur={ ( event, editor ) => {
+                /* console.log( 'Blur.', editor ); */
+            } }
+            onFocus={ ( event, editor ) => {
+                /* console.log( 'Focus.', editor ); */
+            } }
+        />
+    </div>
+    )
+}
+export default Editor
